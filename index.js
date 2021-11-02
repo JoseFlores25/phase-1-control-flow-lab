6212d2d9be0bf3592a551fdd49d3f1551b0a6007
Jose <flores.jose2185@gmail.com>
2021-11-01 23:14:57 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(free) {
+  if (free <= 400) {
+    return "This one is on me!";
+  }
+  if (free > 2000) {
+    if (free > 2500) {
+      return "No can do.";
+    }
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  const nyc = "NYC";
+  if (cityName === nyc) {
+    return "Ok, sounds good.";
+  }
+  if (cityName !== nyc) {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
